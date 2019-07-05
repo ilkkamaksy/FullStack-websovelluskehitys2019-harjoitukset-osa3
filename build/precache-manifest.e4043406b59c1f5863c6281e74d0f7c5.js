b37b323f298b7a07e6c315547eceda3887227903
ilkkamaksy <ilkka.makinen@suuntastudios.com>
2019-07-05 16:10:19 +0200

    'uibuild'

---
+++ build/precache-manifest.e4043406b59c1f5863c6281e74d0f7c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35789d7a5d41e50199ba6cb275c9b44e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f623b0bd70ebd458e8c",
+    "url": "/static/css/main.2d46fec8.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "3f623b0bd70ebd458e8c",
+    "url": "/static/js/main.e41ec05a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
